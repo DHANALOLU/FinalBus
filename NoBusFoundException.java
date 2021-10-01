68b1bbc54d57c336525b2593eadd0d524c225be4
DHANALOLU SANTHOSHINI <67041673+DHANALOLU@users.noreply.github.com>
2021-10-01 13:14:23 +0200

    Create NoBusFoundException.java

---
+++ NoBusFoundException.java
@@ -0,0 +1,14 @@
+
+public class NoBusFoundException extends Exception{
+
+
+	private static final long serialVersionUID = 1L;
+	NoBusFoundException()
+	{
+		super();
+	}
+	NoBusFoundException(String str)
+	{
+		super(str);
+	}
+}
